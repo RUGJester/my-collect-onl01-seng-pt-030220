86e4f5c0bda42c50e9a8d7d5eb227e5345e36993
Sean Melton <seanlape@gmail.com>
2020-03-11 20:15:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+list = ["Red Skittles", "Blue MMs", "Timmy Dude"]
 
+
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this wa
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
 
